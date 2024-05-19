9505922a0336082899b89fa14db43f7d3a09f7d6
Muhammad Ibrahim Mubashir <160342010+MuhammadIbrahimMubashir@users.noreply.github.com>
2024-05-19 22:15:46 +0500

    Add files via upload

---
+++ Class 8/8.ts
@@ -0,0 +1,30 @@
+//Code(Arrow Function one line code):-
+/*let multiply = (num1,num2) => num1 * num2
+console.log(multiply(2,6))*/
+
+//Function (Nasted):-
+/*function yourname(){
+    let x = "Muhammad Ibrahim Mubashir"
+    console.log(x)
+
+    function num(){
+        let y = 2
+        console.log(y)
+    }
+    num()
+}
+yourname()*/
+
+//Code(Variable with all datatype):-
+/*let name1 : string | number | boolean | undefined = "Hello"
+name1 = 1
+name1 = true
+console.log(name1)*/
+
+//Code
+/*To Open Tsconfigation Flie
+tsc --init
+
+For watching the file
+tsc -w
+on cmd*/

bbd2c64ddf50afff2acb776a890c7c3716922abb
Muhammad Ibrahim Mubashir <160342010+MuhammadIbrahimMubashir@users.noreply.github.com>
2025-03-27 17:43:17 +0500

    Update app.py

--- Q3/Classes/Class 02/app.py
+++ Q3/Classes/Class 02/app.py
@@ -139,3 +139,154 @@ weight: float = 66.89
 print("check: isinstance(age, int)      = ", isinstance(age, int))
 print("check: isinstance(weight, int)   = ", isinstance(weight, int))
 print("check: isinstance(weight, float) = ", isinstance(weight, float))
+
+print("Lesson 03")
+print("Operators,Keywords & Variables")
+
+# x + y = z
+# x and y are operands
+# + - * / are operators
+# z is results
+
+# Unary Operators
+#1.Negative
+x = 5
+y = -x # y is -5
+print(y)
+
+#2.Logical NOT (not)
+a = True
+b = not a  # b is now False
+print("b =", b)
+
+# Bitwise NOT (~)
+x: int = 5  # Binary: 0101
+y: int = ~x  # y is now -6 (binary: 1010, but in two's complement form)
+print("y =", y)
+
+# Binary Operators
+#1.Arithmetic Operators (+, -, *, /, etc.):
+#2.Perform basic math operations.
+#3.Comparison Operators (==, !=, >, <, etc.):
+#4.Compare two values and return True or False.
+#5.Logical Operators (and, or):
+#6.Combine boolean values.
+#7.Assignment Operators (=, +=, -=, etc.):
+#8.Assign values to variables or perform operations while assigning.
+
+#1.Arithmetic Operators:-
+#1. +	Addition	5 + 2 = 7
+#2. -	Subtraction	5 - 2 = 3
+#3. *	Multiplication	5 * 2 = 10
+#4. /	Division (float)	5 / 2 = 2.5
+#5. //	Floor Division	5 // 2 = 2 (removes decimal part)
+#6. %	Modulus (remainder)	5 % 2 = 1
+#7. **	Exponentiation	5 ** 2 = 25
+
+a: int = 10
+b: int = 3
+print("a + b  = ", a + b)   # 13 Addition
+print("a - b  = ", a - b)   # 7 Subtraction
+print("a * b  = ", a * b)   # 30 Multiplication
+print("a / b  = ", a / b)   # 3.3333333333333335
+print("a // b = ", a // b)  # 3 Floor Division
+print("a % b  = ", a % b)   # 1 Modulus (remainder)
+print("a % b  = ", a ** b)
+
+#2. Comparison (Relational) Operators:-
+# ==	Equal	5 == 5 → True
+# !=	Not equal	5 != 3 → True
+# >	Greater than	5 > 3 → True
+# <	Less than	5 < 3 → False
+# >=	Greater than or equal	5 >= 5 → True
+# <=	Less than or equal	5 <= 3 → False
+
+x: int = 10
+y: int = 5
+
+print("x == y = ", x == y)  # False, Equal
+print("x != y = ", x != y)  # True, Not equal
+print("x > y  = ", x > y)   # True, Greater than
+print("x < y  = ", x < y)   # False, Less than
+print("x >= y = ", x >= y)  # True, Greater than or equal
+print("x <= y = ", x <= y)  # False, Less than or equal
+
+#3. Logical Operators:-
+# and	Logical AND	(5 > 3 and 10 > 5) → True
+# or	Logical OR	(5 > 3 or 10 < 5) → True
+# not	Logical NOT	not(5 > 3) → False
+
+x: bool = True
+y: bool = False
+
+print("x and y = ", x and y)  # False
+print("x or y  = ", x or y)   # True
+print("not x   = ", not x)    # False
+
+#4. Assignment Operators
+# =	x = 5	x = 5
+# +=	x += 3	x = x + 3
+# -=	x -= 3	x = x - 3
+# *=	x *= 3	x = x * 3
+# /=	x /= 3	x = x / 3
+# //=	x //= 3	x = x // 3
+
+x = 5
+print("Assignment: x = 5                    ",x)  # Output: 5
+
+x += 3  # Equivalent to x = x + 3
+print("Addition Assignment: x += 3          ",x)  # Output: 8
+
+x -= 3  # Equivalent to x = x - 3
+print("Subtraction Assignment: x -= 3       ",x)  # Output: 5
+
+x *= 3  # Equivalent to x = x * 3
+print("Multiplication Assignment: x *= 3    ",x)  # Output: 15
+
+x /= 3  # Equivalent to x = x / 3
+print("Division Assignment: x /= 3          ",x)  # Output: 5.0
+
+x //= 3  # Equivalent to x = x // 3
+print("Floor Division Assignment: x //= 3   ",x)  # Output: 1.0
+
+#5. Identity Operators
+#is	Returns True if objects have the same memory location	x is y
+#is not	Returns True if objects do not have the same memory location	x is not y
+
+a: list = [1, 2, 3]
+b: list = [1, 2, 3]
+c: list = a
+
+print("a is c     =  ",a is c)       # True  (same object, sharing same memmory space)
+print("a is b     =  ",a is b)       # False (different objects, seperate memmory space)
+print("a == b     =  ", a == b)      # True  (same values, different memmory space but having same vlaues)
+print("a is not b =  ", a is not b)  # True  (True because of different memmory space, although having same memmory space)
+
+print('\n-----\n') # seperator for better output readability
+
+print("id(a) = ", id(a))
+print("id(b) = ", id(b))
+print("id(c) = ", id(c))
+
+#6. Membership Operators
+#in	Returns True if value is in the sequence	x in list
+#not in	Returns True if value is NOT in the sequence	x not in list
+
+my_list: list = [1, 2, 3, 4, 5]
+
+print("my_list           = ", my_list)            # [1, 2, 3, 4, 5]
+print("3 in my_list      = ", 3 in my_list)       # True
+print("10 not in my_list = ", 10 not in my_list)  # True
+
+print("\n-----\n")
+
+my_string: str = "Operation Badar"
+
+print("my_string                 = ", my_string)                # Operation Badar
+print("'O' in my_string          = ", 'O' in my_string)         # True
+print("'Hello' not in my_string  = ", 'Hello' not in my_string) # True
+
+# Deleting Variable
+x: int = 10
+del x
+print(x)

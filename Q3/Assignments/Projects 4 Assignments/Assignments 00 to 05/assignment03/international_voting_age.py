27bd1861860bba845cdfd72b9942abe0de96cea9
Muhammad Ibrahim Mubashir <160342010+MuhammadIbrahimMubashir@users.noreply.github.com>
2025-04-13 17:06:56 +0500

    Create international_voting_age.py

---
+++ Q3/Assignments/Projects 4 Assignments/Assignments 00 to 05/assignment03/international_voting_age.py
@@ -0,0 +1,19 @@
+def check_voting_eligibility(age):
+    if age >= 16:
+        print("You can vote in Peturksbouipo where the voting age is 16.")
+    else:
+        print("You cannot vote in Peturksbouipo where the voting age is 16.")
+    
+    if age >= 25:
+        print("You can vote in Stanlau where the voting age is 25.")
+    else:
+        print("You cannot vote in Stanlau where the voting age is 25.")
+    
+    if age >= 48:
+        print("You can vote in Mayengua where the voting age is 48.")
+    else:
+        print("You cannot vote in Mayengua where the voting age is 48.")
+
+# Ask the user for their age
+age = int(input("How old are you? "))
+check_voting_eligibility(age)

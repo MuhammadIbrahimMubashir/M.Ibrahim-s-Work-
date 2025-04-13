76ce1dc750010656159447aac51e5c88beb76fba
Muhammad Ibrahim Mubashir <160342010+MuhammadIbrahimMubashir@users.noreply.github.com>
2025-04-13 16:48:55 +0500

    Create seconds_in_year.py

---
+++ Q3/Assignments/Projects 4 Assignments/Assignments 00 to 05/assignment01/seconds_in_year.py
@@ -0,0 +1,11 @@
+# Constants
+days_in_year = 365
+hours_in_day = 24
+minutes_in_hour = 60
+seconds_in_minute = 60
+
+# Calculate the total number of seconds in a year
+seconds_in_year = days_in_year * hours_in_day * minutes_in_hour * seconds_in_minute
+
+# Print the result
+print(f"There are {seconds_in_year} seconds in a year!")

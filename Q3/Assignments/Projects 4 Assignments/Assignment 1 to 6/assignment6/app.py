3fddf838aa9a381f91741f9bbda4aad8867e05c8
Muhammad Ibrahim Mubashir <160342010+MuhammadIbrahimMubashir@users.noreply.github.com>
2025-04-13 18:09:36 +0500

    Update app.py

--- Q3/Assignments/Projects 4 Assignments/Assignment 1 to 6/assignment6/app.py
+++ Q3/Assignments/Projects 4 Assignments/Assignment 1 to 6/assignment6/app.py
@@ -1,6 +1,12 @@
-print("Class 01") 
-print("Lesson 1: Introduction To Python")
+import time
 
+# Ask the user for number of seconds
+seconds = int(input("Enter time in seconds: "))
 
+# Countdown loop
+while seconds > 0:
+    print("Time left:", seconds, "seconds")
+    time.sleep(1)  # Wait for 1 second
+    seconds -= 1
 
-
+print("Time's up! ⏰")

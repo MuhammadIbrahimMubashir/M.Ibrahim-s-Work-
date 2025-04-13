3b6f061294427297729adfce1103f948f06faa58
Muhammad Ibrahim Mubashir <160342010+MuhammadIbrahimMubashir@users.noreply.github.com>
2025-04-13 16:48:03 +0500

    Create rolldice.py

---
+++ Q3/Assignments/Projects 4 Assignments/Assignments 00 to 05/assignment01/rolldice.py
@@ -0,0 +1,15 @@
+import random
+
+# Roll the first die
+die1 = random.randint(1, 6)
+
+# Roll the second die
+die2 = random.randint(1, 6)
+
+# Add the dice values
+total = die1 + die2
+
+# Print the results
+print("Die 1:", die1)
+print("Die 2:", die2)
+print("Total:", total)

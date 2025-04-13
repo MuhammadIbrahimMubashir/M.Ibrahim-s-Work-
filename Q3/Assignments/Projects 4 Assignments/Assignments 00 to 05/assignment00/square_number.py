df4f7a927becf7a4bccb0c0235df701c6c6ef072
Muhammad Ibrahim Mubashir <160342010+MuhammadIbrahimMubashir@users.noreply.github.com>
2025-04-13 16:40:26 +0500

    Create square_number.py

---
+++ Q3/Assignments/Projects 4 Assignments/Assignments 00 to 05/assignment00/square_number.py
@@ -0,0 +1,8 @@
+# Ask the user for a number
+number = float(input("Type a number to see its square: "))
+
+# Calculate the square of the number
+square = number * number
+
+# Print the result
+print(number, "squared is", square)

66824cd32b8cebed10f1f5313855157f1656a589
Muhammad Ibrahim Mubashir <160342010+MuhammadIbrahimMubashir@users.noreply.github.com>
2025-04-13 16:44:56 +0500

    Create feet_to_inches.py

---
+++ Q3/Assignments/Projects 4 Assignments/Assignments 00 to 05/assignment01/feet_to_inches.py
@@ -0,0 +1,8 @@
+# Ask for the number of feet
+feet = float(input("Enter the number of feet: "))
+
+# Convert feet to inches
+inches = feet * 12
+
+# Print the result
+print(f"{feet} feet is equal to {inches} inches.")

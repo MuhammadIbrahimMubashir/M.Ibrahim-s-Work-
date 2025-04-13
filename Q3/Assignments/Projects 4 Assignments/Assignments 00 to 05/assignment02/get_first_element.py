0c1b0a1294c4434852b4a83e6e137e267e6de08d
Muhammad Ibrahim Mubashir <160342010+MuhammadIbrahimMubashir@users.noreply.github.com>
2025-04-13 17:01:18 +0500

    Create get_first_element.py

---
+++ Q3/Assignments/Projects 4 Assignments/Assignments 00 to 05/assignment02/get_first_element.py
@@ -0,0 +1,6 @@
+def get_first_element(lst):
+    print(lst[0])
+
+# Example usage
+lst = [input("Enter first element: "), input("Enter second element: "), input("Enter third element: ")]
+get_first_element(lst)

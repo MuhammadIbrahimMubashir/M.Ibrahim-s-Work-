77223ed7991dd758ad0f128acbd0ebfa69bac745
Muhammad Ibrahim Mubashir <160342010+MuhammadIbrahimMubashir@users.noreply.github.com>
2025-04-13 16:38:58 +0500

    Create how_old_are_they.py

---
+++ Q3/Assignments/Projects 4 Assignments/Assignments 00 to 05/assignment00/how_old_are_they.py
@@ -0,0 +1,13 @@
+# Ages based on the clues
+anton = 21
+beth = anton + 6
+chen = beth + 20
+drew = chen + anton
+ethan = chen
+
+# Print each person's age
+print("Anton is", anton)
+print("Beth is", beth)
+print("Chen is", chen)
+print("Drew is", drew)
+print("Ethan is", ethan)

27d479891b0eee3804f845f846dc7c91189650a5
Muhammad Ibrahim Mubashir <160342010+MuhammadIbrahimMubashir@users.noreply.github.com>
2025-04-13 17:01:56 +0500

    Create get_last_element.py

---
+++ Q3/Assignments/Projects 4 Assignments/Assignments 00 to 05/assignment02/get_last_element.py
@@ -0,0 +1,6 @@
+def get_last_element(lst):
+    print(lst[-1])
+
+# Example usage
+lst = [input("Enter first element: "), input("Enter second element: "), input("Enter third element: ")]
+get_last_element(lst)

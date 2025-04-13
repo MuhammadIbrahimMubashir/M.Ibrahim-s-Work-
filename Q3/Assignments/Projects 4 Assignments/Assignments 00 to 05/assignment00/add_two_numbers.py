a5e3c120644865ab603559c30030af0e8f029a9d
Muhammad Ibrahim Mubashir <160342010+MuhammadIbrahimMubashir@users.noreply.github.com>
2025-04-13 16:35:27 +0500

    Update and rename app.py to add_two_numbers.py

---
+++ Q3/Assignments/Projects 4 Assignments/Assignments 00 to 05/assignment00/add_two_numbers.py
@@ -0,0 +1,11 @@
+# Ask the user to enter the first number
+num1 = int(input("Enter the first number: "))
+
+# Ask the user to enter the second number
+num2 = int(input("Enter the second number: "))
+
+# Add the two numbers
+total = num1 + num2
+
+# Show the result
+print("The sum is:", total)

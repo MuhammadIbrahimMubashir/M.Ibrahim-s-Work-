d3ffa5bf5f944fac4b20b18fd57fe357676d3728
Muhammad Ibrahim Mubashir <160342010+MuhammadIbrahimMubashir@users.noreply.github.com>
2025-04-13 17:24:07 +0500

    Create double_it.py

---
+++ Q3/Assignments/Projects 4 Assignments/Assignments 00 to 05/assignment05/double_it.py
@@ -0,0 +1,7 @@
+# Ask the user to enter a number
+curr_value = int(input("Enter a number: "))
+
+# Loop to double the number until it is 100 or greater
+while curr_value < 100:
+    curr_value = curr_value * 2
+    print(curr_value)

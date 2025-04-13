3551dcb465394629b2c468a35b4fa908b45de0fb
Muhammad Ibrahim Mubashir <160342010+MuhammadIbrahimMubashir@users.noreply.github.com>
2025-04-13 16:36:37 +0500

    Create agreement_bot.py

---
+++ Q3/Assignments/Projects 4 Assignments/Assignments 00 to 05/assignment00/agreement_bot.py
@@ -0,0 +1,5 @@
+# Ask the user for their favorite animal
+animal = input("What's your favorite animal? ")
+
+# Respond with a message using the same animal
+print("My favorite animal is also", animal + "!")

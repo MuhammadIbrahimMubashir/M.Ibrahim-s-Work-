9bb163d522c76525dea4ae742922d93de0cd43ee
Muhammad Ibrahim Mubashir <160342010+MuhammadIbrahimMubashir@users.noreply.github.com>
2025-04-13 16:50:17 +0500

    Create tiny_mad_lib.py

---
+++ Q3/Assignments/Projects 4 Assignments/Assignments 00 to 05/assignment01/tiny_mad_lib.py
@@ -0,0 +1,7 @@
+# Prompt the user for words
+adjective = input("Please type an adjective and press enter: ")
+noun = input("Please type a noun and press enter: ")
+verb = input("Please type a verb and press enter: ")
+
+# Print the fun sentence
+print(f"Code in Place is fun. I learned to program and used Python to make my {adjective} {noun} {verb}!")
